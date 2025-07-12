ab49b4ba9ae45a49e6ff34456fd43dc492af39dd
Geovanny Ribeiro <geovanny.ribeiro@gmail.com>
2025-07-11 23:51:12 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,25 @@
-export * from "./control/Common/BrokerType";
-export * from "./control/Message/MessageControl";
-export * from "./control/Kafka/KafkaMessageControl";
-export * from "./control/SQS/SqsMessageControl";
-export * from "./control/SNS/SnsMessageControl";
-export * from "./control/Message/MessageControlFactory";
-export * from "./control/Common/handleNack";
-export * from "./brokers/Kafka/KafkaControlParams";
-export * from "./brokers/SQS/SqsPublisherSubscriber";
-export * from "./contracts/Message";
-export * from "./contracts/MessageBroker";
+// ===== COMMON =====
+export * from './control/Common/BrokerType';
+export * from './control/Common/ControlParams';
+export * from './control/Common/handleNack';
+
+// ===== MESSAGE CONTROL =====
+export * from './control/Message/MessageControl';
+export * from './control/Message/MessageControlFactory';
+
+// ===== KAFKA =====
+export * from './control/Kafka/KafkaMessageControl';
+export * from './brokers/Kafka/KafkaControlParams';
+export * from './brokers/Kafka/KafkaPublisherSubscriber';
+
+// ===== SQS =====
+export * from './control/SQS/SqsMessageControl';
+export * from './brokers/SQS/SqsControlParams';
+export * from './brokers/SQS/SqsPublisherSubscriber';
+
+// ===== SNS =====
+export * from './control/SNS/SnsMessageControl';
+
+// ===== CONTRACTS =====
+export * from './contracts/Message';
+export * from './contracts/MessageBroker';
